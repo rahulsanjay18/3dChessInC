e4ccd9ac7492520bcbfc9df1a1d503636c2229f6
Rahul Shah <rahulsanjay18@gmail.com>
2025-04-19 13:27:29 -0400

    interface

---
+++ include/game.h
@@ -0,0 +1,9 @@
+#include "coordinates.h"
+#include "game_state.h"
+#ifndef GAME
+#define GAME
+typedef struct Game Game;
+
+void initialize_game();
+bool move(Coordinates* start, Coordinates* end);
+void game_loop();
