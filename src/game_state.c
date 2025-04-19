a1abd61961f2162746bbcf9b7a7f701a66e28c62
Rahul Shah <rahulsanjay18@gmail.com>
2025-04-19 13:25:10 -0400

    small updates

--- src/game_state.c
+++ src/game_state.c
@@ -1,14 +1,13 @@
 #include <stdbool.h>
 #include "constants.h"
 #include "board_group.h"
-#ifndef GAME_STATE
-#define GAME_STATE
-typedef struct GameState GameState;
-char board_representation[BOARD_SIZE][BOARD_SIZE][BOARD_SIZE];
-int captured_pieces[SIZE_OF_CHARACTER_MAP];
-bool is_white_turn;
-bool castle_status[CASTLES];
-bool is_checkmate;
+struct GameState GameState{
+	char board_representation[BOARD_SIZE][BOARD_SIZE][BOARD_SIZE];
+	int captured_pieces[SIZE_OF_CHARACTER_MAP];
+	bool is_white_turn;
+	bool castle_status[CASTLES];
+	bool is_checkmate;
+}
 
 void make_move(Coordinates* start, Coordinates* end);
 void pretty_print_board(char* board[BOARD_SIZE][BOARD_SIZE]);
@@ -16,4 +15,3 @@ int get_piece_num(Coordinates* location);
 char get_piece_char(Coordinates* location);
 void save(char* filepath);
 void load(char* filepath);
-#endif
