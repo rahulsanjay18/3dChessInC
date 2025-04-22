98df003f878533d4d328b847ed17099b70552471
Rahul Shah <rahulsanjay18@gmail.com>
2025-04-22 03:53:31 -0400

    implemented board_group

--- include/board_group.h
+++ include/board_group.h
@@ -1,6 +1,12 @@
 #include "constants.h"
+#include "coordinates.h"
 #include "board.h"
 #ifndef BOARDS
 #define BOARDS
 struct Boards;
+
+Boards* Boards__create(char* board_repr[BOARD_SIZE][BOARD_SIZE]);
+void set_piece(char piece, Coordinates* coordinates);
+void unset_piece(char piece, Coordinates* coordinates);
+char get_piece(Coordinates* coordinates);
 #endif
--- src/board_group.c
+++ src/board_group.c
@@ -10,7 +10,8 @@ struct Boards{
 void Boards__init(Boards* result, char* board_repr[BOARD_SIZE][BOARD_SIZE]){
 	for(int i = 0; i < SIZE_OF_CHARACTER_MAP; i++){
 		char repr = CHARACTER_MAP[i];
-		Board__create(repr, board_repr);
+		Board* board = Board__create(repr, board_repr);
+		resolt->full_board[i] = board;
 	}
 }
 
@@ -18,4 +19,25 @@ Boards* Boards__create(char* board_repr[BOARD_SIZE][BOARD_SIZE]){
 	Boards* result = Boards* malloc(sizeof(Boards));
 	Boards__init(result);
 	return result;
+}
+
+void set_piece(char piece, Coordinates* coordinates){
+	int index = char_to_int(piece);
+	Board* board = full_board[index];
+	set(board, coordinates);
+}
+
+void unset_piece(char piece, Coordinates* coordinates){
+	int index = char_to_int(piece);
+	Board* board = full_board[index];
+	unset(board, coordinates);
+}
+
+char get_piece(Coordinates* coordinates){
+	for(int i = 0; i < SIZE_OF_CHARACTER_MAP; i++){
+		if(get(full_board[i], coordinates)){
+			return CHARACTER_MAP[i];
+		}
+	}
+	return '\0';
 }
